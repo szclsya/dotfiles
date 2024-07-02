51ff69651d0467830a3cc6e195dfdc1279138ecf
Leo Shen <i@szclsya.me>
2024-07-02 16:19:49 -0400

    color scheme stuff

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,12 +4,23 @@ local config = wezterm.config_builder()
 --- Config Starts
 config.font = wezterm.font 'monospace'
 config.font_size = 11
+config.font_rules = {
+  {
+    intensity = "Bold",
+    font = wezterm.font({ family = "monospace", weight = "ExtraBold" }),
+  },
+}
 
-config.color_scheme = 'Tomorrow Night Bright'
 config.window_background_opacity = 0.75
 config.colors = {
+    foreground = "#eaeaea",
+    background = "#000000",
+    ansi = {"#000000","#d54e53","#b9ca4a","#e7c547","#7aa6da","#c397d8","#70c0b1","#ffffff"},
+    brights = {"#666666","#d54e53","#b9ca4a","#e7c547","#7aa6da","#c397d8","#70c0b1","#ffffff"},
     selection_fg = 'black',
-    selection_bg = 'white',
+    selection_bg = '#e7e7e7',
+    cursor_fg = 'black',
+    cursor_bg = 'white',
 }
 
 config.window_padding = {
